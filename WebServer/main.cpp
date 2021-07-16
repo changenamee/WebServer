d6053111b33e7642278fa1511be411593ba3f44d
skyyy <77455095+changenamee@users.noreply.github.com>
2021-07-17 01:10:36 +0800

    Update main.cpp

--- WebServer/main.cpp
+++ WebServer/main.cpp
@@ -7,7 +7,6 @@ int main() {
         1316, 3, 60000, false,        
         //mysql
         3306, "root", "aqaa", "mydb", 
-        //连接池数量 线程池数量 日志开关 日志等级
         //connNum threadNum
         12, 6);            
     server.Start();

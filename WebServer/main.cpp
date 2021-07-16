743e51d23b84090775abc7afa912c92ddb412ddd
skyyy <77455095+changenamee@users.noreply.github.com>
2021-07-17 01:10:19 +0800

    Update main.cpp

--- WebServer/main.cpp
+++ WebServer/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "webserver.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    WebServer server(
+        //端口 trigger模式，timeout，linger
+        1316, 3, 60000, false,        
+        //mysql
+        3306, "root", "aqaa", "mydb", 
+        //连接池数量 线程池数量 日志开关 日志等级
+        //connNum threadNum
+        12, 6);            
+    server.Start();
     return 0;
 }
